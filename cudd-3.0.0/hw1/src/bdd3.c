15ef4a6f2969509e5e83096f007d92dbe465d8e5
Bohan Wang <bohanw@umich.edu>
2017-01-21 00:44:55 -0500

    bddprob1_3

--- cudd-3.0.0/hw1/src/bdd3.c
+++ cudd-3.0.0/hw1/src/bdd3.c
@@ -41,18 +41,32 @@ void write_dd (DdManager *gbm, DdNode *dd, char** inames, char** onames, char* f
     fclose (outfile);                       // close the file 
 }
 
-
+/*
+ * Build subfunction to AND four variables based on input value
+ * and merge with BDD
+ */
 DdNode*  bddAndFour(DdManager *gbm, DdNode *arr[], int val0, int val1, int val2, int val3) 
 {
     DdNode *res;
     DdNode *var, *tmp;
-    int i;
+    int i,j;
     res = Cudd_ReadOne(gbm);
     Cudd_Ref(res);
+	
+    int val[4];
+    val[0] = val0;
+    val[1] = val1;
+    val[2] = val2;
+    val[3] = val3;
+
     for(i = 3; i>=0;i--) 
     {
-	var = Cudd_bddIthVar(gbm,i);
-	tmp = Cudd_bddAnd(gbm,arr[i],res);
+	if (val[i]) {
+	    tmp = Cudd_bddAnd(gbm,arr[i],res);
+	} else {
+	    //var = Cudd_Not(arr[i]);
+	    tmp = Cudd_bddAnd(gbm,Cudd_Not(arr[i]),res);
+	}
 	Cudd_Ref(tmp);
 	Cudd_RecursiveDeref(gbm,res);
 	res = tmp;
@@ -65,10 +79,11 @@ int main (int argc, char *argv[])
 {
     DdManager *gbm;         /* Global BDD manager. */
     char filename[30];      /* File name for output file */
-    DdNode *f,*tmp,*tmp_neg, *f1, *f2, *f3, *f4, *f5, *f6;
+    DdNode *bdd,*tmp,*var;
     DdNode *x[3];
+    DdNode *f0, *f1, *f2, *f3, *f4, *f5;
     char * inames[4] = { "x0", "x1","x2","x3" };     /* Names for input variables */
-    char * onames[1] = { "f" };          /* Name for output variable */
+    char * onames[1] = { "bdd" };          /* Name for output variable */
 
     /* Initialize the bdd manager with default options */
     gbm = Cudd_Init(0,0,CUDD_UNIQUE_SLOTS,CUDD_CACHE_SLOTS,0);
@@ -78,21 +93,75 @@ int main (int argc, char *argv[])
     for (int i=0;i<4;i++) {
         x[i] = Cudd_bddNewVar(gbm);
     }
-
-    f = Cudd_bddNewVar(gbm);
+    //f = Cudd_bddNewVar(gbm);
     
+    //f1 = Cudd_bddNewVar(gbm);
+    //f2 = Cudd_bddNewVar(gbm);
     /* ordering = x < y */
-    int prod1[4], prod2[4], prod3[4], prod5[4], prod6[4];
+  
 
-    f = bddAndFour(gbm,x,);
 
-    f = Cudd_BddToAdd(gbm, f);                          /*Convert BDD to ADD for display purpose*/
-    print_dd (gbm, f, 2,4);                    /*Print the DD to standard output*/
+    f0 = bddAndFour(gbm,x,0,0,1,1);
+    f1 = bddAndFour(gbm,x,0,1,0,1); 
+    f2 = bddAndFour(gbm,x,0,1,1,0);
+    f3 = bddAndFour(gbm,x,1,0,0,1);
+    f4 = bddAndFour(gbm,x,1,0,1,0);
+    f5 = bddAndFour(gbm,x,1,1,0,0);
+
+    
+    /*
+    tmp = Cudd_bddOr(gbm, f1, f2);
+    f = tmp;
+    Cudd_Ref(tmp);
+    Cudd_RecursiveDeref(gbm, f);*/
+    tmp = Cudd_bddOr(gbm, f0,f1);
+    bdd = tmp;
+    //bdd = Cudd_ReadOne(gbm);
+    Cudd_Ref(bdd);
+    Cudd_RecursiveDeref(gbm,bdd);
+
+    tmp = Cudd_bddOr(gbm, bdd,f2);
+    bdd = tmp;
+    //bdd = Cudd_ReadOne(gbm);
+    Cudd_Ref(bdd);
+    Cudd_RecursiveDeref(gbm,bdd);
+
+    tmp = Cudd_bddOr(gbm, bdd,f3);
+    bdd = tmp;
+    //bdd = Cudd_ReadOne(gbm);
+    Cudd_Ref(bdd);
+    Cudd_RecursiveDeref(gbm,bdd);
+
+    tmp = Cudd_bddOr(gbm, bdd,f4);
+    bdd = tmp;
+    //bdd = Cudd_ReadOne(gbm);
+    Cudd_Ref(bdd);
+    Cudd_RecursiveDeref(gbm,bdd);
+
+
+    tmp = Cudd_bddOr(gbm, bdd,f5);
+    bdd = tmp;
+    //bdd = Cudd_ReadOne(gbm);
+    Cudd_Ref(bdd);
+    Cudd_RecursiveDeref(gbm,bdd);
+
+
+/*
+    for(int k = 0;k<=2;k++) {
+	tmp = Cudd_bddOr(gbm,f[k], bdd);
+	Cudd_Ref(tmp);
+	Cudd_RecursiveDeref(gbm,bdd);
+	bdd = tmp;
+
+    }*/
+
+    bdd = Cudd_BddToAdd(gbm, bdd);                          /*Convert BDD to ADD for display purpose*/
+    print_dd (gbm, bdd, 4,4);                    /*Print the DD to standard output*/
     sprintf(filename, "./dot/bdd3.dot");                /*Write .dot filename to a string*/
-    write_dd(gbm, f, (char **)inames, (char **)onames, filename);   /*Write the resulting cascade DD to a file*/
-    printf("Number of Nodes = %d\n",Cudd_DagSize(f));
+    write_dd(gbm, bdd, (char **)inames, (char **)onames, filename);   /*Write the resulting cascade DD to a file*/
+    printf("Number of Nodes = %d\n",Cudd_DagSize(bdd));
 
-    Cudd_RecursiveDeref(gbm,f);        /* Explicit Dereference */
+    Cudd_RecursiveDeref(gbm,bdd);        /* Explicit Dereference */
     Cudd_Quit(gbm);
     return 0;
 }
